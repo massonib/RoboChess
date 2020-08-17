4bb2af7470a74a9d288103e6e5de882035bf37c5
Brandon Massoni <brmassoni@gmail.com>
2020-08-17 14:25:09 -0700

    Remove unused code

--- Denso/DensoControls.WUO/DensoControls.WUO/Source Files/simplemove.pac
+++
@@ -1,7 +0,0 @@
-'!TITLE "<Title>"
-PROGRAM SimpleMove
-
-TakeArm 
-MOVE P, P[57]
-
-END
--- Denso/DensoControls.WUO/DensoControls.WUO/Source Files/var_tab.h
+++
@@ -1,8 +0,0 @@
-'===== Type I Variables =====
-'===== Type F Variables =====
-'===== Type D Variables =====
-'===== Type V Variables =====
-'===== Type P Variables =====
-'===== Type J Variables =====
-'===== Type T Variables =====
-'===== Type S Variables =====
